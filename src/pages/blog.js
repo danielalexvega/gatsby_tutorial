81d97fc88da00516f723cd585d8fc6e0c09af6ae
Daniel Vega <danielalexvega@gmail.com>
2020-11-12 23:10:17 -0600

    styled components example

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,6 +5,8 @@ const blog = () => {
   return (
     <Layout>
       <h1>This is our blog page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iure molestias exercitationem soluta tempora distinctio vel labore nihil aspernatur accusantium, sequi corrupti dolores perspiciatis. Ratione?</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa aperiam eum repellendus iste architecto sit pariatur adipisci exercitationem minima ad libero ratione sapiente quis, omnis, temporibus in neque saepe, eos earum consequuntur aspernatur! Quisquam error qui excepturi laboriosam assumenda exercitationem libero voluptatibus, architecto voluptates ipsum tempore eveniet nulla. Sint, animi.</p>
     </Layout>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import styles from '../components/products.module.css';
 
 const products = () => {
   return (
     <Layout>
-      <h1>This is our blog page</h1>
+      <h1>This is our Products page</h1>
     </Layout>
   )
 }
