1056e6977ba6186ef6dadc88138729fbeeba8463
Daniel Vega <danielalexvega@gmail.com>
2020-11-22 16:11:52 -0600

    work on products page with contentful

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import styles from '../components/products.module.css';
+import React from "react"
+import { graphql } from "gatsby"
 
-const products = () => {
-  return (
-    <Layout>
-      <div className={styles.page}>
-        <h1>This is our Products page</h1>
-        <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi ab quos, minus repudiandae, impedit modi ipsum eius cum repellendus aperiam aspernatur nisi. Quae ea sequi, sint voluptas dolore blanditiis itaque?Praesentium neque quis earum voluptatibus, velit, officia, in perspiciatis excepturi amet minima culpa facere unde repellendus inventore eius hic omnis magnam aliquam accusamus recusandae voluptate! Consequuntur quod ut quas necessitatibus.</p>
-      </div>
-    </Layout>
-  )
-}
+const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
 
-export default products;
+export const query = graphql`
+  {
+    allContentfulProduct {
+      nodes {
+        title
+        price
+        slug
+        image {
+          fluid {
+            ...GatsbyContentfulFluid
+          }
+        }
+      }
+    }
+  }
+`
+
+export default ComponentName
